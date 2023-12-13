b3243088ba2e29c884619b49c7439d80ac290b86
Gabriel Macus <gabrielmacus@gmail.com>
2023-12-13 11:40:49 -0300

    Added function to separate subquerys from query

--- ez_rest/modules/query/services.py
+++ ez_rest/modules/query/services.py
@@ -1,6 +1,45 @@
+from sqlalchemy import or_, and_
+import re
+
+#  "name eq 'John' and (name eq 'A' or (name eq 'B' or name eq 'Z')) or name eq 'Jane' and surname eq 'Doe'"
+
+
 class QueryServices:
     
-    def split_filter_members(self, query:str):
-        members = [m.replace("").strip() for m in query.split('and')]
+    def get_queries(self, query:str, members:dict = None):
+        if members is None:
+            members = {}
+
+        pattern = r'\(([^()]+)\)'
+        matches = re.finditer(pattern, query)
+
+        query_arr = list(query)
+        # The length of the query changes as members are replaced
+        # by references ({number}, so an offset is declared
+        # to make further replacements
+        offset = 0
+        index = len(members.keys())
+        for match in matches:
+            start = match.span()[0] - offset
+            end = match.span()[1] - offset
+
+            key = '{'+str(index)+'}'
+            query_arr[start:end] = key
+            offset = len(match.group()) - len(key)
+            members[index] = match.group(1)
+            index += 1
+
+        new_query = "".join(query_arr)
+        
+        if(new_query != query):
+            return self.get_queries(new_query, members)
+        
+        # If last member has only a member reference, it means that
+        # the original query was inside brackets.
+        # In that case, this member is ignored.
+        if re.match(r'^{[0-9]{1,}}$', new_query) is None:
+            members[index] = new_query
+
         return members
+        
     
--- tests/unit/query/test_unit__query_services.py
+++ tests/unit/query/test_unit__query_services.py
@@ -1,8 +1,49 @@
 import pytest
 from ez_rest.modules.query.services import QueryServices
 
-  "name eq 'John' or name eq 'Jane' and surname eq 'Doe'"
+@pytest.mark.parametrize("query, expected_members", [
 
+    (
+        "(age gte 18 and role eq 'Admin') or (age lt 18 and role ne 'Admin')",
+        {
+            0:"age gte 18 and role eq 'Admin'",
+            1:"age lt 18 and role ne 'Admin'",
+            2:"{0} or {1}"
+        }
+    ),
+    (
+        "(age gte 18 and role eq 'Admin') or age lt 18",
+        {
+            0:"age gte 18 and role eq 'Admin'",
+            1:"{0} or age lt 18"
+        }
+    ),
+    (
+        "name eq 'John' or surname eq 'Doe' and (name eq 'A' or (name eq 'B' or name eq 'Z')) or name eq 'Jane' and surname eq 'Doe' and (name eq 'Foo' or name eq 'Bar')",
+        {
+            0:"name eq 'B' or name eq 'Z'",
+            1:"name eq 'Foo' or name eq 'Bar'",
+            2:"name eq 'A' or {0}",
+            3:"name eq 'John' or surname eq 'Doe' and {2} or name eq 'Jane' and surname eq 'Doe' and {1}"
+        }
+    ),
+    (
+        "(name eq 'John' or surname eq 'Doe' and (name eq 'A' or (name eq 'B' or name eq 'Z')) or name eq 'Jane' and surname eq 'Doe' and (name eq 'Foo' or name eq 'Bar'))",
+        {
+            0:"name eq 'B' or name eq 'Z'",
+            1:"name eq 'Foo' or name eq 'Bar'",
+            2:"name eq 'A' or {0}",
+            3:"name eq 'John' or surname eq 'Doe' and {2} or name eq 'Jane' and surname eq 'Doe' and {1}"
+        }
+    )
+])
+def test_get_queries(query, expected_queries):
+    services = QueryServices()
+    queries = services.get_queries(query)
+    
+    assert queries == expected_queries
+
+'''
 @pytest.mark.parametrize("query_str,members",[
     (
         "name eq 'John' and surname eq 'Doe'", 
@@ -21,3 +62,4 @@ def test_split_filter_members(query_str, members):
     
     services = QueryServices()
     assert members == services.split_filter_members(query_str)
+'''
