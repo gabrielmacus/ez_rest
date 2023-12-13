94a39c159579bd83739c9ef0129af6ac3fc9eeb9
Gabriel Macus <gabrielmacus@gmail.com>
2023-12-13 11:45:35 -0300

    get_queries docs

--- ez_rest/modules/query/services.py
+++ ez_rest/modules/query/services.py
@@ -1,14 +1,28 @@
 from sqlalchemy import or_, and_
 import re
 
-#  "name eq 'John' and (name eq 'A' or (name eq 'B' or name eq 'Z')) or name eq 'Jane' and surname eq 'Doe'"
-
-
 class QueryServices:
     
-    def get_queries(self, query:str, members:dict = None):
-        if members is None:
-            members = {}
+    def get_queries(self, 
+                    query:str, 
+                    queries:dict[str,str] = None) -> dict[str,str]:
+        """Gets main query and subqueries with references.
+        Example: "(age gte 18 and role eq 'Admin') or (age lt 18 and role ne 'Admin')"
+        Returns: {
+                    0:"age gte 18 and role eq 'Admin'",
+                    1:"age lt 18 and role ne 'Admin'",
+                    2:"{0} or {1}"
+                 }
+
+        Args:
+            query (str): Main query
+            queries (dict[str,str], optional): Accumulated queries. Defaults to None.
+
+        Returns:
+            dict[str,str]: Dict with main query and subqueries with references
+        """
+        if queries is None:
+            queries = {}
 
         pattern = r'\(([^()]+)\)'
         matches = re.finditer(pattern, query)
@@ -18,7 +32,7 @@ class QueryServices:
         # by references ({number}, so an offset is declared
         # to make further replacements
         offset = 0
-        index = len(members.keys())
+        index = len(queries.keys())
         for match in matches:
             start = match.span()[0] - offset
             end = match.span()[1] - offset
@@ -26,20 +40,20 @@ class QueryServices:
             key = '{'+str(index)+'}'
             query_arr[start:end] = key
             offset = len(match.group()) - len(key)
-            members[index] = match.group(1)
+            queries[index] = match.group(1)
             index += 1
 
         new_query = "".join(query_arr)
         
         if(new_query != query):
-            return self.get_queries(new_query, members)
+            return self.get_queries(new_query, queries)
         
         # If last member has only a member reference, it means that
         # the original query was inside brackets.
         # In that case, this member is ignored.
         if re.match(r'^{[0-9]{1,}}$', new_query) is None:
-            members[index] = new_query
+            queries[index] = new_query
 
-        return members
+        return queries
         
     
